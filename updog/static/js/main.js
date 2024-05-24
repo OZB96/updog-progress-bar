3e21a6b3a3a717af0337627b304982c30166caaa
Omar <omarzbazaid@gmail.com>
2024-05-24 16:17:01 +0300

    Progress Bar Added

--- updog/static/js/main.js
+++ updog/static/js/main.js
@@ -13,6 +13,37 @@ $(document).ready(function () {
 	});
 });
 
+function uploadFile() {
+var file = document.getElementById('file').files[0];
+var formData = new FormData();
+formData.append("file", file);
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "/", true);
+
+xhr.upload.onprogress = function(event) {
+	if (event.lengthComputable) {
+	var percentComplete = (event.loaded / event.total) * 100;
+	var progressBar = document.getElementById('progress-bar');
+	var progressBarAlt = document.getElementById('progress-bar-alt');
+	progressBar.style.width = percentComplete + '%';
+	progressBar.innerHTML = Math.round(percentComplete) + '%';
+	progressBarAlt.style.width = percentComplete + '%';
+	}
+};
+
+xhr.onload = function() {
+	if (xhr.status == 200) {
+	alert('File uploaded successfully.');
+	} else {
+	alert('File upload failed.');
+	}
+};
+
+xhr.send(formData);
+}
+
+
 var inputs = document.querySelectorAll( '.uploadFile' );
 
 Array.prototype.forEach.call( inputs, function( input )
